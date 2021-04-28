4195379847941970a9d03a13c47ab668d43f4651
Jackson Tian <shyvo1987@gmail.com>
2021-04-28 13:10:58 +0800

    remove mkdirp, expect.js; improve test cases

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,24 @@
 'use strict';
 
+const assert = require('assert');
 const os = require('os');
-const expect = require('expect.js');
 
 const Footprint = require('../');
 
 describe('footprint', function () {
   it('Footprint.YYYYMMDD(date) should ok', function () {
-    var day = new Date(2016, 11, 22);
-    expect(Footprint.YYYYMMDD(day)).to.be('20161222');
-    day = new Date(2017, 0, 1);
-    expect(Footprint.YYYYMMDD(day)).to.be('20170101');
+    const day1 = new Date(2016, 11, 22);
+    assert.deepStrictEqual(Footprint.YYYYMMDD(day1), '20161222');
+    const day2 = new Date(2017, 0, 1);
+    assert.deepStrictEqual(Footprint.YYYYMMDD(day2), '20170101');
   });
 
   it('new Footprint should ok', function () {
     var footprint = new Footprint();
-    expect(footprint).to.be.ok();
     var YYYYMMDD = Footprint.YYYYMMDD(new Date());
     footprint.log('hi');
     var expectFilepath = `${os.tmpdir()}/footprint-${YYYYMMDD}.log`;
     var filepath = footprint.stream.path;
-    expect(filepath).to.be(expectFilepath);
+    assert.deepStrictEqual(filepath, expectFilepath);
   });
 });
